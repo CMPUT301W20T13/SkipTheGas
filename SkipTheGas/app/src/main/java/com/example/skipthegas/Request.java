7fd3b5ab1e9d4fa9e867b627dfa2119627824e37
Nan Samy <nan.p2198@gmail.com>
2020-03-11 17:50:15 -0600

    added functionality to request class

--- SkipTheGas/app/src/main/java/com/example/skipthegas/Request.java
+++ SkipTheGas/app/src/main/java/com/example/skipthegas/Request.java
@@ -2,34 +2,91 @@ package com.example.skipthegas;
 
 //import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.firestore.GeoPoint;
 
 public class Request {
-    private LatLng riderCurrentLocation;
-    private LatLng riderDestination;
+    private GeoPoint origin;
+    private GeoPoint destination;
     private String riderName;
-    private String riderNumber;
+    private String riderPhone;
+    private String riderEmail;
+    private String driverName;
+    private String driverPhone;
+    private String driverEmail;
+    private String dist;
+    private String time;
+    private String fare;
+    private boolean accepted;
+    private boolean completed;
 
-    Request(String riderName, String riderNumber, LatLng riderCurrentLocation, LatLng riderDestination) {
+    Request(String riderName, String riderPhone, String riderEmail, GeoPoint origin,
+            GeoPoint destination, String dist, String time, String fare,
+            String driverName, String driverPhone, String driverEmail, boolean accepted, boolean completed) {
         this.riderName = riderName;
-        this.riderNumber = riderNumber;
-        this.riderCurrentLocation = riderCurrentLocation;
-        this.riderDestination = riderDestination;
+        this.riderPhone = riderPhone;
+        this.riderEmail = riderEmail;
+        this.origin = origin;
+        this.destination = destination;
+        this.dist = dist;
+        this.time = time;
+        this.fare = fare;
+        this.driverName = driverName;
+        this.driverPhone = driverPhone;
+        this.driverEmail = driverEmail;
+        this.accepted = accepted;
+        this.completed = completed;
     }
 
     String getRiderName() {
         return this.riderName;
     }
 
-    String getRiderNumber() {
-        return this.riderNumber;
+    String getRiderPhone() {
+        return this.riderPhone;
     }
 
-    LatLng getRiderCurrentLocation() {
-        return this.riderCurrentLocation;
+    String getRiderEmail() {
+        return this.riderEmail;
     }
 
-    LatLng getRiderDestination() {
-        return this.riderDestination;
+    String getDriverName() {
+        return this.driverName;
+    }
+
+    String getDriverPhone() {
+        return this.driverPhone;
+    }
+
+    String getDriverEmail() {
+        return this.driverEmail;
+    }
+
+    GeoPoint getOrigin() {
+        return this.origin;
+    }
+
+    GeoPoint getDestination() {
+        return this.destination;
+    }
+
+    String getDist() {
+        return this.dist;
+    }
+
+    String getTime() {
+        return this.time;
+    }
+
+    String getFare() {
+        return this.fare;
+    }
+
+    Boolean getAccepted() {
+        return this.accepted;
+    }
+
+    Boolean getCompleted() {
+        return this.completed;
     }
 }
 
