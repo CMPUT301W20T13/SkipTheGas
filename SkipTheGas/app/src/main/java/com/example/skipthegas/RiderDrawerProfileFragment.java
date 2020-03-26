fad398421b0856a668d799ce202d9ba00d0515b7
Nan Samy <nan.p2198@gmail.com>
2020-03-26 16:59:27 -0600

    implementing drawer menu for rider

---
+++ SkipTheGas/app/src/main/java/com/example/skipthegas/RiderDrawerProfileFragment.java
@@ -0,0 +1,108 @@
+package com.example.skipthegas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.Objects;
+
+/**
+ * This fragment is accessed from within the Rider's drawer menu
+ * It displays the rider's profile information fetched from firebase
+ */
+public class RiderDrawerProfileFragment extends Fragment {
+    TextView riderNameEditText;
+    TextView riderPhoneEditText;
+    TextView riderEmailEditText;
+    TextView riderProfileHeader;
+
+    Button editButton;
+
+    FirebaseFirestore firebaseFirestore;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+
+    String riderName;
+    String riderPhone;
+    String riderEmail;
+
+    /**
+     * onCreateView method for RiderDrawerProfileFragment fragment
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_rider_profile, container, false);
+        return view;
+    }
+
+    /**
+     * onActivityCreated method for DriverDrawerProfileFragment fragment
+     * Get rider's information from firebase and display it on profile
+     * @param savedInstanceState
+     */
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        riderNameEditText = getActivity().findViewById(R.id.rider_profile_user_name_textView);
+        riderPhoneEditText = getActivity().findViewById(R.id.rider_profile_phone_TextView);
+        riderEmailEditText = getActivity().findViewById(R.id.rider_profile_email_TextView);
+        riderProfileHeader = getActivity().findViewById(R.id.rider_profile_header);
+
+        editButton = getActivity().findViewById(R.id.editButton);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        riderEmail = firebaseUser.getEmail();
+        firebaseFirestore
+                .collection("users")
+                .document(Objects.requireNonNull(riderEmail))
+                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    /**
+                     * Method sets the edit text fields in the edit rider profile page
+                     * @param documentSnapshot
+                     * @param e
+                     */
+                    @Override
+                    public void onEvent(@javax.annotation.Nullable DocumentSnapshot documentSnapshot, @javax.annotation.Nullable FirebaseFirestoreException e) {
+                        riderName = documentSnapshot.getString("username");
+                        riderPhone = documentSnapshot.getString("phone");
+                        String header = riderName + "'s Profile";
+                        riderNameEditText.setText(riderName);
+                        riderPhoneEditText.setText(riderPhone);
+                        riderEmailEditText.setText(riderEmail);
+                        riderProfileHeader.setText(header);
+                    }
+                });
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent editProfileIntent = new Intent(getActivity(), RiderProfileEditable.class);
+                startActivity(editProfileIntent);
+            }
+        });
+
+    }
+}
---
+++ SkipTheGas/app/src/main/java/com/example/skipthegas/RiderMapFragment.java
@@ -0,0 +1,153 @@
+package com.example.skipthegas;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+/**
+ * Rider's map activity fragement
+ * Rider can view map from this fragment
+ */
+public class RiderMapFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private View mapView;
+
+    private boolean mLocationPermissionGranted = false;
+    private static final String TAG = "RiderMapFragment";
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 9001;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    /**
+     * onCreateView method for the Rider map fragment
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_rider_map, null, false);
+        initMap();
+        return view;
+    }
+
+    /**
+     * This method initializes the map in the rider map fragment
+     */
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.rider_map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+        mapView = mapFragment.getView();
+    }
+
+    /**
+     * onMapReady method to set current location in rider mao fragment
+     * Sets 'my current location' button to the bottom right corner
+     * @param googleMap
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            getDeviceLocation();
+            mMap.setMyLocationEnabled(true);
+        }
+
+        if (mapView != null && mapView.findViewById(Integer.parseInt("1")) != null) {
+            View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            layoutParams.setMargins(0, 0, 30, 30);
+        }
+
+    }
+
+    /**
+     * Get devices current location for rider map fragment
+     */
+    private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: getting the devices location");
+
+        FusedLocationProviderClient mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        try {
+            final Task location = mFusedLocationProviderClient.getLastLocation();
+            location.addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "onComplete: found location!");
+                        Location currentLocation = (Location) task.getResult();
+
+                        assert currentLocation != null;
+                        moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+
+                    } else {
+                        Log.d(TAG, "onComplete: current location is null");
+                        Toast.makeText(getActivity(), "unable to get current location", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
+        }
+    }
+
+    /**
+     * This method moves the camera to the marked points on the map
+     * @param latLng
+     * @param zoom
+     */
+    private void moveCamera(LatLng latLng, float zoom) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    /**
+     * Get device permission to get current location
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+    }
+}
