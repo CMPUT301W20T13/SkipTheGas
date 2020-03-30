2b57f630cc391b4b455402bc465b93329c0ee502
hz6 <hz6@ualberta.ca>
2020-03-29 21:02:16 -0600

    driver rating updated

--- SkipTheGas/app/src/main/java/com/example/skipthegas/PaymentActivity.java
+++ SkipTheGas/app/src/main/java/com/example/skipthegas/PaymentActivity.java
@@ -1,8 +1,20 @@
 package com.example.skipthegas;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 
 /**
@@ -13,9 +25,51 @@ public class PaymentActivity extends AppCompatActivity {
      * onCreate method for PaymentActivity class
      * @param savedInstanceState
      */
+
+    ImageView QR_Image;
+    Button ratingButton;
+    QRCodeWriter writer;
+    double fare;
+    Ride currentRide;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment_layout);
+
+        // TODO: ********** ********** GET FARE FROM THE CURRENT RIDE ********** **********
+        fare = 25.0;// Default fare
+        // TODO: ********** ********** GET FARE FROM THE CURRENT RIDE ********** **********
+
+        QR_Image = findViewById(R.id.imageView5);
+        ratingButton = findViewById(R.id.rating_button);
+        writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode(Double.toString(fare), BarcodeFormat.QR_CODE,500,500,null);
+            int height = bitMatrix.getHeight();
+            int width = bitMatrix.getWidth();
+
+            // Create a Bitmap image with the same size as bitMatrix
+            Bitmap bmp = Bitmap.createBitmap(width,height,Bitmap.Config.RGB_565);
+
+            // Fill up the Bitmap bmp with the value of BitMatrix bitMatrix
+            for (int x=0; x<width; x++) {
+                for (int y=0; y<height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+
+            // show the bitmap on the screen
+            QR_Image.setImageBitmap(bmp);
+
+        } catch (WriterException we) {
+            Toast.makeText(this, "Unable to generate QR-code", Toast.LENGTH_SHORT).show();
+            we.printStackTrace();
+        }
+    }
+
+    public void goToRating(View view) {
+        Intent feedBackIntent = new Intent(getApplicationContext(),TripFeedBackActivity.class);
+        startActivity(feedBackIntent);
     }
 }
--- SkipTheGas/app/src/main/java/com/example/skipthegas/TripFeedBackActivity.java
+++ SkipTheGas/app/src/main/java/com/example/skipthegas/TripFeedBackActivity.java
@@ -1,6 +1,12 @@
 package com.example.skipthegas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +15,12 @@ import androidx.appcompat.app.AppCompatActivity;
  * trip and gives the rider an opportunity to rate the driver
  */
 public class TripFeedBackActivity extends AppCompatActivity {
+    Button returnButton;
+    ToggleButton goodRating;
+    ToggleButton badRating;
+
+    TextView goodRatingCnt;
+    TextView badRatingCnt;
 
     /**
      * onCreate method for TripFeedBackActivity class
@@ -18,5 +30,36 @@ public class TripFeedBackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.trip_feedback_layout);
+        goodRating = findViewById(R.id.imageView);
+        badRating = findViewById(R.id.imageView2);
+
+        goodRatingCnt = findViewById(R.id.textView33);
+        badRatingCnt = findViewById(R.id.textView34);
+
+        returnButton = findViewById(R.id.rating_button);
+
+        goodRating.setOnClickListener(view -> {
+            badRating.setChecked(false);
+            goodRating.setChecked(true);
+        });
+        
+        badRating.setOnClickListener(view -> {
+            goodRating.setChecked(false);
+            badRating.setChecked(true);
+        });
+
+        returnButton.setOnClickListener(view -> {
+            if (!badRating.isChecked() && !goodRating.isChecked()) {
+                Toast.makeText(this, "Please rate your driver", Toast.LENGTH_SHORT).show();
+            } else {
+
+                // TODO: ********** ********** Update the driver ratings in Firebase ********** **********
+                /* TODO */
+                // TODO: ********** ********** Update the driver ratings in Firebase ********** **********
+                Intent riderIntent = new Intent(getApplicationContext(),RiderDrawerActivity.class);
+                startActivity(riderIntent);
+                finish();
+            }
+        });
     }
 }
