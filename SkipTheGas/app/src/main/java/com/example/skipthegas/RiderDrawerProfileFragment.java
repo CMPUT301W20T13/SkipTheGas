c9698a780b5ceedc00daad8667a6772f7109e1c3
hz6 <hz6@ualberta.ca>
2020-04-01 16:33:30 -0600

    rider logout exception handled

--- SkipTheGas/app/src/main/java/com/example/skipthegas/RiderDrawerProfileFragment.java
+++ SkipTheGas/app/src/main/java/com/example/skipthegas/RiderDrawerProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.skipthegas;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,6 +42,7 @@ public class RiderDrawerProfileFragment extends Fragment {
     String riderName;
     String riderPhone;
     String riderEmail;
+    String TAG = "RiderDrawerProfileFragment:";
 
     /**
      * onCreateView method for RiderDrawerProfileFragment fragment
@@ -75,7 +77,9 @@ public class RiderDrawerProfileFragment extends Fragment {
         firebaseFirestore = FirebaseFirestore.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
-        riderEmail = firebaseUser.getEmail();
+        if (firebaseUser!=null) {
+            riderEmail = firebaseUser.getEmail();
+        }
         firebaseFirestore
                 .collection("users")
                 .document(Objects.requireNonNull(riderEmail))
@@ -87,13 +91,21 @@ public class RiderDrawerProfileFragment extends Fragment {
                      */
                     @Override
                     public void onEvent(@javax.annotation.Nullable DocumentSnapshot documentSnapshot, @javax.annotation.Nullable FirebaseFirestoreException e) {
-                        riderName = documentSnapshot.getString("username");
-                        riderPhone = documentSnapshot.getString("phone");
-                        String header = riderName + "'s Profile";
-                        riderNameEditText.setText(riderName);
-                        riderPhoneEditText.setText(riderPhone);
-                        riderEmailEditText.setText(riderEmail);
-                        riderProfileHeader.setText(header);
+                        if (e!=null){
+                            Log.i(TAG,"Error occurred "+e.getMessage());
+                            return;
+                        }
+                        if (documentSnapshot!=null && documentSnapshot.exists()) {
+                            riderName = documentSnapshot.getString("username");
+                            riderPhone = documentSnapshot.getString("phone");
+                            String header = riderName + "'s Profile";
+                            riderNameEditText.setText(riderName);
+                            riderPhoneEditText.setText(riderPhone);
+                            riderEmailEditText.setText(riderEmail);
+                            riderProfileHeader.setText(header);
+                        } else {
+                            Log.d(TAG,"document does not exist");
+                        }
                     }
                 });
         editButton.setOnClickListener(new View.OnClickListener() {
